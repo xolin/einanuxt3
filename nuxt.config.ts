458eb06f2c348279ddad61439b2b542013c39b86
Joan Aznar <jaznar@kiteris.com>
2022-11-30 15:37:46 +0100

    add nitro vercel preset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,14 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    target: 'static',
     css: ['~/assets/css/main.css'],
     postcss: {
         plugins: {
           tailwindcss: {},
           autoprefixer: {},
         },
+    },
+    nitro: {
+      preset: "vercel",
     },
     // modules: [
     //   'nuxt-headlessui'
