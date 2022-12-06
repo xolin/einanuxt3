2ac557014458052500e499dd34cb389b0f57489c
Joan Aznar <jaznar@kiteris.com>
2022-12-06 05:28:49 +0100

    ssr false

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    css: ['~/assets/css/main.css'],
+  ssr: false,  
+  css: ['~/assets/css/main.css'],
     postcss: {
         plugins: {
           tailwindcss: {},
