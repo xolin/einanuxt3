bff9c629342220aea9c4a477ad80cc590e5c398c
Joan Aznar <jaznar@kiteris.com>
2022-12-13 01:47:20 +0100

    Add LayersList component

---
+++ components/LayersList.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="w-full px-4 pt-16 layers-list items-end">
+      <div class="mx-auto w-full max-w-md rounded-l bg-white p-2">
+        <ul v-for="layer in layers" :key="layer.id">
+            <li @click="selectObjectFromList(layer.id)">{{layer.id}} - {{layer.type}}
+                <button class="icon mr-4 cursor-pointer" @click="toggleHideObjectFromList(layer.id, 'hide')" v-if="layer.opacity > 0.1">Hide</button>
+                <button class="icon mr-4 cursor-pointer" @click="toggleHideObjectFromList(layer.id, 'show')" v-if="layer.opacity == 0">Show</button>
+            </li>
+        </ul>
+      </div>
+    </div>
+</template>
+  
+<script setup>
+  import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/vue'
+  import { ChevronUpIcon } from '@heroicons/vue/20/solid'
+  import { ref, shallowRef } from 'vue'
+  const { $bus } = useNuxtApp();
+
+const props = defineProps({
+    layers: Array
+})
+
+function selectObjectFromList(selectedId) {
+    $bus.$emit('selectObjectFromList', selectedId)
+}
+
+function toggleHideObjectFromList(selectedId, action) {
+    $bus.$emit('toggleHideObjectFromList', {selectedId, action})
+}
+</script>
+  
+<style scoped>
+li {
+    border: 1px solid red;        
+}
+.icon {
+    position: relative;
+    float: right;
+}
+.layers-list {
+    position: absolute;
+    right: 10px;
+    z-index: 1;
+}
+</style>
