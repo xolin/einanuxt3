37fc7da4e43ed8293ef86779967e84bd2e76defa
Joan Aznar <jaznar@kiteris.com>
2022-12-15 03:36:40 +0100

    Clean

--- components/Editor.vue
+++ components/Editor.vue
@@ -19,24 +19,14 @@
             </div>
         </div>
         <div class="options--top-left cursor-pointer" @click="toggleLayersList()">
-            <Bars4Icon
-              class="h-5 w-5 text-purple-500"
-            />
-
+            <Bars4Icon class="h-5 w-5 text-purple-500"/>
         </div>
         <div class="textedit--top">
             <div class="inline-block colorPickerWrapper">
                 <input type="color" @input="fontColorChange($event)" />
             </div>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" class="w-6 h-6">
-                <path fill-rule="evenodd" d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75H12a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z" clip-rule="evenodd" />
-            </svg>
-
-
-
         </div>
     
-        
         <div class="textedit--bottom ">
             <fontfamilytool
                 v-for="fontFamily in fontFamilyAvailable"
