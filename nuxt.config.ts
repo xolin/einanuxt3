0717224060593cc7281d44110d20782a090ca4e2
Joan Aznar <jaznar@kiteris.com>
2023-01-16 01:46:09 +0100

    Fix bug: avoid resize at input text using mobile

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  ssr: false,  
+  ssr: false,
+    app: {
+    head: {
+      charset: 'utf-8',
+      meta: [
+        { name: 'viewport', content: 'width=device-width, height=device-height, initial-scale=1, minimum-scale=1, maximum-scale=1' }
+      ]
+    }
+  },
   css: ['~/assets/css/main.css'],
     postcss: {
         plugins: {
